a2e99ee98598e2829d987799887475b2f16e17b4
chamesbenrezigue <chames.benrezigue@esprit.tn>
2021-03-31 02:57:20 +0200

    bug

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class ProfilController extends AbstractController
 {
@@ -22,18 +24,19 @@ class ProfilController extends AbstractController
     /**
      * @Route("/profil_edit", name="edit_profil")
      */
-    public function editPrfoil(Request $request): Response
+    public function editPrfoil(Request $request,UserPasswordEncoderInterface  $encoder): Response
     {
             $user =$this->getUser();
             $form = $this->createForm(EditProfilType::class, $user );
             $form->handleRequest($request);
 
             if ($form->isSubmitted() && $form->isValid()) {
+                $hash= $encoder->encodePassword($user, $user->getPassword());
+                $user->setPassword($hash);
                 $entityManager = $this->getDoctrine()->getManager();
-                $confirmerPassword= $user->getPassword();
-                $ax = $user->setConfirmPassword($confirmerPassword);
 
-                $entityManager->persist($user,$ax);
+
+                $entityManager->persist($user);
                 $entityManager->flush();
                 $this->addFlash('message','ADD');
                 return $this->redirectToRoute('profil');
